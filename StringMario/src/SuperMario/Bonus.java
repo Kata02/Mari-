c572bc72338a5bc1d9d332e49762070b80d8160f
Kata <kata.gallambos@gmail.com>
2017-08-26 12:12:14 +0200

    Mashrum, star, 1 up, money
    Máriónak akár szerencséje is lehet, ha találkozik a gombával( Mashrum) "nagyobbra nő" Super Marióvá válik, de ha megsérül, akkor visszanyeri az eredeti formáját, de nem veszít életet. Ha a csillaggal(Star) találkozik sérthetetlenné válik a következő lépésekben. Ha 1 up, akkor +1 életet kap. Valamint ha kellő pénzérmét(money) gyűlyt össze (100), szintén +1 életet kap.

---
+++ StringMario/src/SuperMario/Bonus.java
@@ -0,0 +1,56 @@
+package SuperMario;
+
+public class Bonus {
+	private String nev; //ermek, gomba, star, 1 up
+	private int ermek;		//100erem +1 elet
+	
+	private boolean szuper;	//gomba által nagyobbra nõ, sérüléskor lecsökken
+	private boolean serthetetlen;	//csillag 2 lépés sérthetetlenseg
+	private int upp;	//plusz élet
+	
+	public String getNev()
+	{
+		return nev;
+	}
+	public boolean isSzuper() {
+		return szuper;
+	}
+
+	public int getErmek() {
+		return ermek;
+	}
+	public boolean isSerthetetlen() {
+		return serthetetlen;
+	}
+	public int getUpp() {
+		return upp;
+	}
+
+	public Bonus(String nev, int ermek, int szuper, int serthetetlen,int upp) {
+		this.nev = nev;
+		this.ermek += ermek;
+		if(ermek == 100)
+		{
+			Ellenseg.getLive();
+		}
+		if( szuper == 1 )
+		{
+			this.szuper = true;
+		}
+		else
+		{
+			this.szuper = false;
+		}	
+		
+		if( serthetetlen == 1 )
+		{
+			this.serthetetlen = true;
+		}
+		else
+		{
+			this.serthetetlen = false;
+		}
+		this.upp = upp;
+		//Ellenseg.live+upp;
+	}
+}
