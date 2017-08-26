c02cc3a8e5af7c6217bd3b5c2e4ec06e46f6c699
Marcell <marcell@neszt.hu>
2017-08-26 14:30:42 +0200

    Basic code cleanup
    remove unecessary enters and autoformat code

--- StringMario/src/SuperMario/Ellenseg.java
+++ StringMario/src/SuperMario/Ellenseg.java
@@ -1,42 +1,35 @@
 package SuperMario;
 
 public class Ellenseg {
-	
-	private String nev; //coopa, piranja, goomba
-	private static int live=3;
-	private boolean dead;
-	
-	public String getNev()
-	{
-		return nev;
-	}
-	
-	public static int getLive()
-	{
-		return live;
-	}
-	public boolean isDead()
-	{
-		return dead;
-	}
-	
-	public Ellenseg(String nev, int live, int dead)
-	{
-		this.nev = nev;
-		Ellenseg.live += live;
-		if(Ellenseg.live == 0)
-		{
-			dead = 1;
-		}
-		
-		if( dead == 1 )
-		{
-			this.dead = true;
-		}
-		else
-		{
-			this.dead = false;
-		}
-	}
+
+    private String nev; //coopa, piranja, goomba
+    private static int live = 3;
+    private boolean dead;
+
+    public String getNev() {
+        return nev;
+    }
+
+    public static int getLive() {
+        return live;
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public Ellenseg(String nev, int live, int dead) {
+        this.nev = nev;
+        Ellenseg.live += live;
+        if (Ellenseg.live == 0) {
+            dead = 1;
+        }
+
+        if (dead == 1) {
+            this.dead = true;
+        } else {
+            this.dead = false;
+        }
+    }
 
 }
